ab0eab7e776f1eb48de067ff1069ec0ac2da77db
unknown <prakashankit5261@outlook.com>
2024-07-26 23:20:48 +0530

    added ani

--- client/src/pages/OurTeam.js
+++ client/src/pages/OurTeam.js
@@ -8,33 +8,44 @@ import moonImg from '../assets/images/team/moon.jpg'
 import kabirImg from '../assets/images/team/kabir.jpg'
 import santanuImg from '../assets/images/team/santanu.jpg'
 import asisImg from '../assets/images/team/asis.jpg'
+import { Fade } from 'react-awesome-reveal'
 
 export default function OurTeam() {
-  return (
-    <div className='z-0 tracking-wide md:tracking-widest'>
-        <Navbar page="our-team" />
+    return (
+        <div className='z-0 tracking-wide md:tracking-widest'>
+            <Navbar page="our-team" />
 
-        <div className='section px-3 md:px-24 lg:px-48 py-4 md:py-8 lg:py-10 mt-[5.8rem]'>
-            <h1 className='text-2xl md:text-4xl text-center md:mb-4 mt-4'>MEET OUR TEAM</h1>
-            <h2 className='text-xl md:text-2xl text-center mb-4 md:mb-8 italic'>"Alone we can do so little; together we can do so much."</h2>
+            <div className='section px-3 md:px-24 lg:px-48 py-4 md:py-8 lg:py-10 mt-[5.8rem]'>
+                <Fade direction="up" triggerOnce>
 
-            <div className='md:h-[32rem] flex justify-center'>
-                <TeamCard img={anjuImg} name="MRS. ANJUMANARA BEGAM" position="MANAGING DIRECTOR" />
+                    <h1 className='text-2xl md:text-4xl text-center md:mb-4 mt-4'>MEET OUR TEAM</h1>
+                    <h2 className='text-xl md:text-2xl text-center mb-4 md:mb-8 italic'>"Alone we can do so little; together we can do so much."</h2>
+                </Fade>
+
+                <Fade direction="up" triggerOnce>
+                    <div className='md:h-[32rem] flex justify-center'>
+
+                        <TeamCard img={anjuImg} name="MRS. ANJUMANARA BEGAM" position="MANAGING DIRECTOR" />
+                    </div>
+                </Fade>
+                <Fade direction="up" triggerOnce>
+
+                    <div className='md:h-[30rem] flex justify-center flex-col md:flex-row mt-12 gap-12'>
+                        <TeamCard img={mampiImg} name="MS. MAMPI MONDAL" position="DIRECTOR" />
+                        <TeamCard img={moonImg} name="MS. MOONMOON MONDAL" position="DIRECTOR" />
+                    </div>
+                </Fade>
+                <Fade direction="up" triggerOnce>
+
+                    <div className='md:h-[28rem] flex justify-center flex-col md:flex-row mt-12 gap-12'>
+                        <TeamCard img={kabirImg} name="MR. KABIR MONDAL" position="OPERATIONAL HEAD" />
+                        <TeamCard img={asisImg} name="MS. MOONMOON MONDAL" position="ASISTANT MANAGER ADMIN" />
+                        <TeamCard img={santanuImg} name="MR. SANTANU CHAUDHARY" position="SENIOR MANAGER ADMIN" />
+                    </div>
+                </Fade>
             </div>
 
-            <div className='md:h-[30rem] flex justify-center flex-col md:flex-row mt-12 gap-12'>
-                <TeamCard img={mampiImg} name="MS. MAMPI MONDAL" position="DIRECTOR" />
-                <TeamCard img={moonImg} name="MS. MOONMOON MONDAL" position="DIRECTOR" />
-            </div>
-
-            <div className='md:h-[28rem] flex justify-center flex-col md:flex-row mt-12 gap-12'>
-                <TeamCard img={kabirImg} name="MR. KABIR MONDAL" position="OPERATIONAL HEAD" />
-                <TeamCard img={asisImg} name="MS. MOONMOON MONDAL" position="ASISTANT MANAGER ADMIN" />
-                <TeamCard img={santanuImg} name="MR. SANTANU CHAUDHARY" position="SENIOR MANAGER ADMIN" />
-            </div>
+            <Footer />
         </div>
-
-        <Footer />
-    </div>
-  )
+    )
 }
